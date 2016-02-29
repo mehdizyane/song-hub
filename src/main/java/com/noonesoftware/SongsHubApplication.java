9d676fdd0ce7e6a6f82384ad11d065db42c2583d
Mehdi ZYANE <Mehdi ZYANE>
2016-02-29 22:19:48 +0100

    First commit

---
+++ src/main/java/com/noonesoftware/SongsHubApplication.java
@@ -0,0 +1,12 @@
+package com.noonesoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongsHubApplication.class, args);
+	}
+}
